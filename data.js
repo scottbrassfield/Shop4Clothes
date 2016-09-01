a11a41b975fac61a42ee9940400570ca7a50a7f9
Scott Brassfield <scott.brassfield@gmail.com>
2016-09-01 09:32:53 -0700

    User can view cart. #3

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ var products = [
     category: 'Sporting Goods',
     name:'Schwinn Mark V Bike',
     description:'Schwinn\'s Mark V is a full-dress cruiser that nails the number one reason we ride bikes: fun.',
-    price: '$150.00',
+    price: 150,
     img:'http://sicklerbikes.com/images/library/zoom/schwinn_MARV26_SND_13_z.jpg'
   },
   {
@@ -12,7 +12,7 @@ var products = [
     category: 'Electronics',
     name:'USB Drive',
     description:'32GB USB Drive',
-    price: '$32.75',
+    price: 32.75,
     img:'#'
   },
   {
@@ -20,7 +20,7 @@ var products = [
     category: 'Auto',
     name:'Car',
     description:'Honda Civic',
-    price: '$15,827.00',
+    price: 15827,
     img:'#'
   },
   {
@@ -28,7 +28,7 @@ var products = [
     category: 'Clothing and Accessories',
     name:'Glasses',
     description:'Tom Ford square-rim glasses',
-    price: '$119.85',
+    price: 119.85,
     img:'#'
   },
   {
@@ -36,7 +36,7 @@ var products = [
     category: 'Electronics',
     name:'Macbook Pro',
     description:'13-in Macbook Pro',
-    price: '1,300.00',
+    price: 1300,
     img:'#'
   },
   {
@@ -44,21 +44,21 @@ var products = [
     category: 'Electronics',
     name:'Dell Ultrabook',
     description:'15-in Dell Ultrabook',
-    price: '$999.99',
+    price: 999.99,
     img:'#'},
   {
     id: 'p007',
     category: 'Electronics',
     name:'HP Pavillion',
     description:'13-in HP Pavillion',
-    price: '$850.00',
+    price: 850,
     img:'#'},
   {
     id: 'p008',
     category: 'School and Office Supplies',
     name:'Paper',
     description:'1 ream of paper',
-    price: '$1.27',
+    price: 1.27,
     img:'#'
   },
   {
@@ -66,7 +66,7 @@ var products = [
     category: 'Clothing and Accessories',
     name:'Nike Roshe Running Shoes',
     description:'Made for both running and everyday use',
-    price: '$95.50',
+    price: 95.50,
     img:'http://images.nike.com/is/image/DotCom/511881_A_V2?&$img=511881_023_A_PREM&$PDP_HERO$'
   },
 
@@ -75,7 +75,7 @@ var products = [
     category: 'Clothing and Accessories',
     name:'Vans High-Tops',
     description:'Classic pair of shoes from Vans',
-    price: '$44.95',
+    price: 44.95,
     img:'http://images.vans.com/is/image/Vans/QG36BT-HERO?$583x583$'
   },
   {
@@ -83,7 +83,7 @@ var products = [
     category: 'Sporting Goods',
     name:'Basketball',
     description:'Spalding indoor-outdoor basketball',
-    price: '$27.00',
+    price: 27.00,
     img:'http://thumbs1.ebaystatic.com/d/l225/m/m9jsV41Jk7HU6fBkgyd9W-g.jpg'
   }
 ]
--- default.js
+++ default.js
@@ -38,36 +38,125 @@ document.addEventListener('click', function(e) {
   }
 });
 
-//event listener for add to cart
+//event listener for adding to cart
 document.addEventListener('click', function(e) {
-  var prodId;
   for (var i=0; i<matches.length; i++) {
-    prodId = e.target.getAttribute('data-id');
-    if (prodId === matches[i].id) {
-      cart.push(prodId);
+    if (e.target.getAttribute('data-id') === matches[i].id) {
+        addToCart(matches[i]);
     }
   }
 })
 
+//event listener for updating cart total based on changes to quantity
+document.addEventListener('change', function(e) {
+  for (var i=0; i<cart.length; i++) {
+    if (e.target.getAttribute('data-id') === cart[i].id) {
+      updateTotal(e, cart[i]);
+    }
+  }
+})
+
+//add item to cart
+function addToCart(obj) {
+  if (cart.length === 0) {
+    obj.quantity = 1;
+    cart.push(obj);
+  }
+  else {
+    var found = false;
+    for (var i=0; i<cart.length; i++) {
+      if (obj.id === cart[i].id) {
+        found = true;
+        cart[i].quantity++;
+        break;
+      }
+    }
+    if(!found) {
+      obj.quantity = 1;
+      cart.push(obj);
+    }
+  }
+}
 
 //clear content and view shopping cart
 function viewCart() {
   clear(content);
-  var displayed;
-  for (var i=0; i<products.length; i++) {
-    displayed = false;
-    for (var k=0; k<cart.length; k++) {
-      if (products[i].id === cart[k]) {
-        if(displayed) {
-          continue;
-        } else {
-          var result = createResult(products[i]);
-          content.appendChild(result);
-          displayed = true;
-        }
-      }
-    }
+  var resultArea = document.createElement('div');
+  resultArea.classList.add('col-md-8');
+  for (var i=0; i<cart.length; i++) {
+    var result = createResult(cart[i]);
+    result.appendChild(quantity(cart[i]));
+    resultArea.appendChild(result);
   }
+  content.appendChild(resultArea);
+  content.appendChild(checkoutBox());
+}
+
+function calculateTotal(array) {
+  var total = 0;
+  for (var i=0; i<array.length; i++) {
+    total += array[i].quantity * array[i].price * 100;
+  }
+  return total / 100;
+}
+
+function updateTotal(e, obj) {
+  var summary = document.getElementById('summary');
+  content.removeChild(summary);
+  obj.quantity = e.target.value;
+  content.appendChild(checkoutBox());
+}
+
+function checkoutBox() {
+  var subtotalText = document.createElement('span');
+  subtotalText.classList.add('order-summary-text');
+  subtotalText.textContent = 'Items:';
+  var subtotalValue = document.createElement('span');
+  subtotalValue.classList.add('order-summary-value');
+  var itemTotal = calculateTotal(cart)
+  subtotalValue.textContent = itemTotal.toLocaleString('en-US',{style: 'currency', currency: 'USD'});
+  var subtotal = document.createElement('div');
+  subtotal.classList.add('subtotal');
+  subtotal.appendChild(subtotalText);
+  subtotal.appendChild(subtotalValue);
+
+  var taxText = document.createElement('span');
+  taxText.classList.add('order-summary-text');
+  taxText.textContent = 'Tax:';
+  var taxValue = document.createElement('span');
+  taxValue.classList.add('order-summary-value');
+  var taxTotal = itemTotal * 8 / 100;
+  taxValue.textContent = taxTotal.toLocaleString('en-US',{style: 'currency', currency: 'USD'});
+  var tax = document.createElement('div');
+  tax.appendChild(taxText);
+  tax.appendChild(taxValue);
+
+  var totalText = document.createElement('span');
+  totalText.classList.add('order-summary-text');
+  totalText.textContent = 'Total:';
+  var totalValue = document.createElement('span');
+  totalValue.classList.add('order-summary-value');
+  var totalCost = itemTotal + taxTotal;
+  totalValue.textContent = totalCost.toLocaleString('en-US',{style: 'currency', currency: 'USD'});
+  var total = document.createElement('div');
+  total.classList.add('checkout-total', 'subtotal');
+  total.appendChild(totalText);
+  total.appendChild(totalValue);
+
+  var checkout = document.createElement('button');
+  checkout.classList.add('checkout-btn');
+  checkout.textContent = 'Checkout';
+
+  var summary = document.createElement('div');
+  summary.classList.add('order-summary', 'col-md-2');
+  summary.setAttribute('id','summary');
+  summary.textContent = 'Cart Summary';
+  summary.appendChild(subtotal);
+  summary.appendChild(tax);
+  summary.appendChild(total);
+  summary.appendChild(checkout);
+
+  return summary;
 }
 
 //perform search based on search criteria
@@ -91,9 +180,9 @@ function clear(element) {
 function compare(products, properties) {
   var added;
   for (var i=0; i<products.length; i++) {
-    var added = false;
+    added = false;
     for (var k=0; k<properties.length; k++) {
-      if (products[i][properties[j]].toLowerCase().indexOf(searchTerm.value.toLowerCase()) === -1) {
+      if (products[i][properties[k]].toLowerCase().indexOf(searchTerm.value.toLowerCase()) === -1) {
         continue;
       } else {
         if (added) {
@@ -109,16 +198,20 @@ function compare(products, properties) {
 
 //display results
 function display(array) {
+  var resultArea = document.createElement('div');
+  resultArea.classList.add('col-md-8');
+
   var resultDetail = document.createElement('div');
   resultDetail.textContent = 'Your search returned ' + array.length + ' results';
-  resultDetail.classList.add('result-detail', 'col-md-9');
+  resultDetail.classList.add('result-detail', 'col-md-8');
   content.appendChild(resultDetail);
 
   for (var i=0; i < array.length; i++) {
     var result = createResult(array[i]);
     result.appendChild(cartBtn(array[i]));
-    content.appendChild(result)
+    resultArea.appendChild(result);
   }
+  content.appendChild(resultArea);
 }
 
 //create result element based on matches array
@@ -133,7 +226,7 @@ function createResult (obj) {
     name.textContent = obj.name;
     name.classList.add('result-name');
     var price = document.createElement('div');
-    price.textContent = obj.price;
+    price.textContent = obj.price.toLocaleString('en-US',{style: 'currency', currency: 'USD'});
     price.classList.add('result-price')
     var descr = document.createElement('div');
     descr.textContent = obj.description;
@@ -146,17 +239,46 @@ function createResult (obj) {
 
     //combine and return result element
     var result = document.createElement('div');
-    result.classList.add('result', 'col-md-9');
+    result.classList.add('result');
     result.appendChild(img);
     result.appendChild(resultText);
     return result;
   }
 
+//create button for adding products to cart
 function cartBtn(obj) {
-  //create button for adding products to cart
   var cartBtn = document.createElement('button');
   cartBtn.textContent = 'Add to Cart';
   cartBtn.classList.add('result-add');
   cartBtn.setAttribute('data-id', obj.id);
   return cartBtn;
+}
+
+function quantity(obj) {
+  var quantLabel = document.createElement('div');
+  quantLabel.classList.add('quant-label');
+  quantLabel.textContent = 'Quantity:'
+  var quantBtn = document.createElement('select');
+  quantBtn.classList.add('quant-btn');
+  quantBtn.setAttribute('value', obj.quantity);
+  quantBtn.setAttribute('data-id', obj.id);
+  for (var i=1; i<=10; i++) {
+    var theOption = document.createElement('option');
+    theOption.setAttribute('value', i);
+    theOption.textContent = i;
+    if (theOption.textContent === obj.quantity.toString()) {
+      theOption.setAttribute('selected', 'selected');
+    }
+    quantBtn.appendChild(theOption);
+  }
+  // var arrow = document.createElement('i');
+  // arrow.classList.add('fa', 'fa-angle-double-down');
+  // arrow.setAttribute('aria-hidden', 'true');
+  // quantBtn.appendChild(arrow);
+
+  var quant = document.createElement('div');
+  quant.classList.add('quant');
+  quant.appendChild(quantLabel);
+  quant.appendChild(quantBtn);
+  return quant;
 }
